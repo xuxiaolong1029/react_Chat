2163ef8f86d50a82657e348f67ba08ba84520dba
xuxl <xuxl@citex.io>
2019-09-10 21:57:50 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware,compose} from 'redux';
 import thunk from 'redux-thunk';
-import {Provider} from 'react-redux'
+import {Provider} from 'react-redux';
+import {BrowserRouter,Route,Link} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import{counter} from './index.redux';
@@ -12,8 +13,20 @@ const store = createStore(counter,compose(
     applyMiddleware(thunk),
     window.devToolsExtension?window.devToolsExtension():f=>f
 ));
+function sun(){
+    return <h1>张大彪</h1>
+}
+function er(){
+    return <h1>张大彪</h1>
+}
 ReactDOM.render(
-    (<Provider store={store}><App/></Provider>),
+    (<Provider store={store}>
+        <BrowserRouter>
+        <Route path='/' exact component={App}></Route>
+        <Route path='/er' component={er}></Route>
+        <Route path='/sun' component={sun}></Route>
+        </BrowserRouter>
+    </Provider>),
     document.getElementById('root')
 );
 //store.subscribe(render); //订阅一次 （react 单向绑定）
