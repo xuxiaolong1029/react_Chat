a47b70b27a289b24e27b54721aaf1c7999d3bc82
xuxl <xuxl@citex.io>
2019-09-12 20:35:59 +0800

    commit

---
+++ src/Auth.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import {login,getUserData} from './Auth.redux'
+
+@connect(
+    state=>state.auth,
+    {login,getUserData}
+)
+
+class Auth extends React.Component{
+    componentDidMount(){
+       this.props.getUserData()
+    }
+    render(){
+        return(
+            <div>
+                <h2>名字:{this.props.user},年龄：{this.props.age}</h2>
+                {this.props.isAuth?<Redirect to='/dashboard' />:null}
+                <h2>你没有权限，需要登录</h2>
+                <button onClick={this.props.login}>登录</button>
+            </div>
+        )
+    }
+}
+export default Auth;
--- src/index.js
+++ src/index.js
@@ -5,44 +5,30 @@ import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {BrowserRouter,Route,Link,Switch} from 'react-router-dom';
 import './index.css';
-import App from './App';
 import{counter} from './index.redux';
+
+import App from './App';
+import Auth from './Auth';
 
 //const reduxDevtools = window.devToolsExtension?window.devToolsExtension
 const store = createStore(counter,compose(
     applyMiddleware(thunk),
     window.devToolsExtension?window.devToolsExtension():f=>f
 ));
-function sun(){
-    return <h1>孙德胜</h1>
-}
-function er(props){
-    console.info(props)
-    return <h1>张大彪</h1>
-}
-class Test extends React.Component{
-    render(){
-        console.log(this.props.match.params.location)
-        return <h1>测试{this.props.match.params.location}</h1>
-    }
-}
 ReactDOM.render(
     (<Provider store={store}>
         <BrowserRouter>
             <div>
                 <ul>
                     <li>
-                        <Link to='/'>独立团</Link>
-                        <Link to='/er'>张大彪</Link>
-                        <Link to='/sun'>孙德胜</Link>
+                        <Link to='/'>首页</Link>
+                        <Link to='/auth'>登录</Link>
                     </li>
                 </ul>
             </div>
             <Switch>
                 <Route path='/' exact component={App}></Route>
-                <Route path='/er' component={er}></Route>
-                <Route path='/sun' component={sun}></Route>
-                <Route path='/:location' component={Test}></Route>
+                <Route path='/auth' component={Auth}></Route>
             </Switch>
         </BrowserRouter>
     </Provider>),
