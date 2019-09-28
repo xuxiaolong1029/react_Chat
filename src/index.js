9e56fb35a5131fabcf7c586ad6f9b8e26a60a92f
xuxl <xuxl@citex.io>
2019-09-28 20:59:25 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+//react常用插件：immutable（优化react）  reselect(优化redux)
+//服务端渲染
+/*
+*  RenderToString和RenderToStaticMarkup(react16前)
+*  RenderToNodeStream(react16)
+* 客户端hydrate取代render
+* */
+//react项目 SSR实战，build代码后的事情
+/*
+* 后node使用babel-node 配置node里的react环境
+* 修改客户端代码，抽离App组件，前后端共享
+* 服务端生成DOM结构，渲染，加载build后的css和js
+* */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware,compose} from 'redux';
